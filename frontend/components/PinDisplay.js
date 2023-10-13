5eb7a9478998e9f04bcbec1d4e3ba2ef365f1874
Tolujoh-n <tolujohnofficial@gmail.com>
2023-10-13 03:18:32 +0100

    pin display updated

--- frontend/components/PinDisplay.js
+++ frontend/components/PinDisplay.js
@@ -1,25 +1,22 @@
 import React, { useState } from "react";
 
 // function PinDisplay({ pin }) {
-  function PinDisplay() {
+function PinDisplay() {
   return (
-    <section id="about-boxes" className="about-boxes">
+    <div className="container calenderly" data-aos="fade-up">
       <br></br>
-      <div className="container calenderly" data-aos="fade-up">
-        <br></br>
-        <div className="calendar-container">
-          <h1 id="calender-title">Your Partage Pin</h1>
-          <div className="pin-display">
-            <h5>
-              Save this Pin or take a screenshot of it as your asset maybe lost
-              when you lost your pin
-            </h5>
-            {/* <p>{pin}</p> */}
-            <p>1234</p>
-          </div>
+      <div className="calendar-container">
+        <h1 id="calender-title">Your Partage Pin</h1>
+        <h5>
+          Save this Pin or take a screenshot of it as your asset maybe lost when
+          you lost your pin
+        </h5>
+        <div className="pin-display">
+          {/* <p>{pin}</p> */}
+          <p>1234</p>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
